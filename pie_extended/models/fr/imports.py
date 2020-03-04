f8d74709c0689e5dcb5b28e06d2ae15f7c33c548
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-03-04 16:57:52 +0100

    imports and so on

--- pie_extended/models/fr/imports.py
+++ pie_extended/models/fr/imports.py
@@ -1,4 +1,4 @@
-from .processor import FrRulesProcessor, FrGlueProcessor
+from .processor import FrGlueProcessor
 from pie_extended.pipeline.postprocessor.proto import RenamedTaskProcessor
 from .tokenizer import FrMemorizingTokenizer
 from pie_extended.pipeline.iterators.proto import DataIterator, GenericExcludePatterns
@@ -7,16 +7,13 @@ from pie_extended.pipeline.postprocessor.memory import MemoryzingProcessor
 
 def get_iterator_and_processor():
     tokenizer = FrMemorizingTokenizer()
-    processor = FrRulesProcessor(
-        apply_on_reinsert=True,
-        head_processor=
-        MemoryzingProcessor(
+    processor = MemoryzingProcessor(
             tokenizer_memory=tokenizer,
             head_processor=FrGlueProcessor(
                 head_processor=RenamedTaskProcessor({"pos": "POS", "NOMB": "NOMB.", "PERS": "PERS."})
             )
         )
-    )
+
     iterator = DataIterator(
         tokenizer=tokenizer
     )

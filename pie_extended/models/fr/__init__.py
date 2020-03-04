c65327314372ab31d9eb6173655c832faaf99e6c
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-03-04 12:41:05 +0100

    models links

--- pie_extended/models/fr/__init__.py
+++ pie_extended/models/fr/__init__.py
@@ -11,8 +11,8 @@ DESC = Metadata(
 )
 
 DOWNLOADS = [
-        File("TODO: Zenodo DL link", "pos-morph.tar"),
-        File("TODO: Zenodo DL link", "lemma.tar")
+        File("https://zenodo.org/record/3696676/files/fr-class-pos-wembs_aux-pos-2020_03_03-18_52_52.tar?download=1", "pos-morph.tar"),
+        File("https://zenodo.org/record/3696684/files/fr-class-lemma-2020_01_20-15_39_25.tar?download=1", "lemma.tar")
 ]
 
 

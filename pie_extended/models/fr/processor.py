aee76c9a4a3dd609e83f214b5937bf520e4e85c4
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-03-04 17:24:41 +0100

    glue fix

--- pie_extended/models/fr/processor.py
+++ pie_extended/models/fr/processor.py
@@ -1,21 +1,44 @@
 import regex as re
-from typing import Dict, Pattern
+from typing import Dict, Generator, Pattern
 
 from pie_extended.pipeline.postprocessor.glue import GlueProcessor
 from pie_extended.pipeline.postprocessor.rulebased import RuleBasedProcessor
 from pie_extended.pipeline.postprocessor.proto import RenamedTaskProcessor
 
 
-
-
 class FrGlueProcessor(GlueProcessor):
     """ We glue morphological features into one column
-
     """
     OUTPUT_KEYS = ["form", "lemma", "POS", "morph"]
     GLUE = {"morph": ["MODE", "TEMPS", "PERS.", "NOMB.", "GENRE", "CAS"]} #, "DEGRE"]}
-    EMPTY_TAG: Dict[str, str] = {"CAS": "x", "NOMB.": "x", "DEGRE": "x", "MODE": "x", "TEMPS": "x", "GENRE": "x",
-                                 "PERS.": "x"}
+    EMPTY_TAG: Dict[str, str] = {"CAS": "CAS=x", "NOMB.": "NOMB.=x", "MODE": "MODE=x", "TEMPS": "TEMPS=x",
+                                 "GENRE": "GENRE=x", "PERS.": "PERS.=x"}
+
+    # We need to change the definition of this function, because, for now,
+    # the model gives us an output like
+    # MODE=imp
+    # and not
+    # imp
+    def _yield_annotation(
+            self,
+            token_dict: Dict[str, str]
+        ) -> Generator[str, None, None]:
+        # For each key we should return
+        for head in self._out:
+            if head not in self._glue:
+                yield head, token_dict[head]
+            else:
+                # Otherwise, we glue together things that should be glued together
+                joined = self._glue_char.join([
+                    # HERE
+                    #glued_task + "=" +
+                    token_dict[glued_task]
+                    for glued_task in self._glue[head]
+                    if token_dict[glued_task] != self._empty_tags.get(glued_task, None)
+                ])
+                if not joined:
+                    joined = self._glue_empty[head]
+                yield head, joined
 
     def __init__(self, *args, **kwargs):
         super(FrGlueProcessor, self).__init__(*args, **kwargs)

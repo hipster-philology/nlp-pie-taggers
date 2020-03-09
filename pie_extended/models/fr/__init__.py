711eafcc2cec8ee1c1ac4786ebbd06a250f3c8c0
Jean-Baptiste Camps <jbcamps@hotmail.com>
2020-03-09 13:14:01 +0100

    new models for morph and the rest

--- pie_extended/models/fr/__init__.py
+++ pie_extended/models/fr/__init__.py
@@ -9,12 +9,24 @@ DESC = Metadata(
 )
 
 DOWNLOADS = [
-        File("https://zenodo.org/record/3696676/files/fr-class-pos-wembs_aux-pos-2020_03_03-18_52_52.tar?download=1", "pos-morph.tar"),
-        File("https://zenodo.org/record/3696684/files/fr-class-lemma-2020_01_20-15_39_25.tar?download=1", "lemma.tar")
+        File("https://zenodo.org/record/3701294/files/fr-class-wembs-lemma-2020_03_06-01_52_24.tar?download=1", "lemma.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-pos-wembs_aux-pos-2020_03_03-18_52_52.tar?download=1", "pos.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-MODE-2020_03_08-17_46_05.tar?download=1", "mode.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-TEMPS-2020_03_08-19_03_26.tar?download=1", "temps.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-PERS-2020_03_08-18_24_29.tar?download=1", "pers.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-NOMB-2020_03_08-17_55_50.tar?download=1", "nomb.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-GENRE-2020_03_08-16_54_44.tar?download=1", "genre.tar"),
+        File("https://zenodo.org/record/3701320/files/fr-class-morph-wembs_aux-CAS-2020_03_08-16_23_57.tar?download=1", "cas.tar")
 ]
 
 
-Models = "<{},pos,MODE,TEMPS,PERS,NOMB,GENRE,CAS><{},lemma>".format(
-        get_path("fr", "pos-morph.tar"),
-        get_path("fr", "lemma.tar")
+Models = "<{},lemma><{},pos><{},MODE><{},TEMPS><{},PERS><{},NOMB><{},GENRE><{},CAS>".format(
+        get_path("fr", "lemma.tar"),
+        get_path("fr", "pos.tar"),
+        get_path("fr", "mode.tar"),
+        get_path("fr", "temps.tar"),
+        get_path("fr", "pers.tar"),
+        get_path("fr", "nomb.tar"),
+        get_path("fr", "genre.tar"),
+        get_path("fr", "cas.tar")
 )

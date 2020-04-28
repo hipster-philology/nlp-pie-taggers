dd854b8b39458b57d6ea5cec46e96a95a8f64857
Thibault Clérice <leponteineptique@gmail.com>
2020-04-28 13:40:13 +0200

    (Model - Lasla) Updated file links

--- pie_extended/models/lasla/__init__.py
+++ pie_extended/models/lasla/__init__.py
@@ -10,10 +10,14 @@ DESC = Metadata(
 )
 
 DOWNLOADS = [
-    File("https://github.com/chartes/deucalion-model-lasla/raw/master/latin-straight.json", "latin-straight.json"),
-    File("https://github.com/chartes/deucalion-model-lasla/raw/master/latin-pos.json", "latin-pos.json"),
-    File("https://github.com/chartes/deucalion-model-lasla/raw/master/latin-needs.json", "latin-needs.json"),
-    File("https://github.com/chartes/deucalion-model-lasla/raw/master/model.tar", "model.tar")
+    File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/0.0.1/latin-straight.json",
+         "latin-straight.json"),
+    File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/0.0.1/latin-pos.json",
+         "latin-pos.json"),
+    File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/0.0.1/latin-needs.json",
+         "latin-needs.json"),
+    File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/0.0.1/model.tar",
+         "model.tar")
 ]
 
 Models = "<{},lemma,Voice,Mood,Deg,Numb,Person,Tense,Case,Gend,pos>".format(get_path("lasla", "model.tar"))

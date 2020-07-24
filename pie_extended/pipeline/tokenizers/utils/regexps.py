eec5eaff62c0408332f787b8a7854f4ed2934080
Thibault Clérice <leponteineptique@gmail.com>
2020-07-24 19:19:39 +0200

    (Cleaning) Shared some resources in tokenizers.utils

---
+++ pie_extended/pipeline/tokenizers/utils/regexps.py
@@ -0,0 +1,6 @@
+RomanNumbers = r"(?:M{1,4}(?:CM|CD|D?C{0,3})(?:XC|XL|L?X{0,3})" \
+               r"(?:IX|IV|V?I{0,3})|M{0,4}(?:CM|C?D|D?C{1,3})" \
+               r"(?:XC|XL|L?X{0,3})(?:IX|IV|V?I{0,3})|M{0,4}" \
+               r"(?:CM|CD|D?C{0,3})(?:XC|X?L|L?X{1,3})" \
+               r"(?:IX|IV|V?I{0,3})|M{0,4}(?:CM|CD|D?C{0,3})" \
+               r"(?:XC|XL|L?X{0,3})(?:IX|I?V|V?I{1,3}))"

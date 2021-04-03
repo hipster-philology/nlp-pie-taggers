e220c0152cf5ae9f4a2ca528a5fcdf62b9870879
Thibault Clérice <leponteineptique@gmail.com>
2021-04-03 09:15:46 +0200

    (LASLA) Updated model with multi-parts (slower )

--- pie_extended/models/lasla/__init__.py
+++ pie_extended/models/lasla/__init__.py
@@ -9,7 +9,7 @@ DESC = Metadata(
     "https://github.com/chartes/deucalion-model-lasla"
 )
 
-VERSION = "0.0.5b"
+VERSION = "0.0.6"
 DOWNLOADS = [
     File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/"+VERSION+"/latin-straight.json",
          "latin-straight.json"),
@@ -17,8 +17,35 @@ DOWNLOADS = [
          "latin-pos.json"),
     File("https://raw.githubusercontent.com/PonteIneptique/latin-lasla-models/"+VERSION+"/latin-needs.json",
          "latin-needs.json"),
-    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/"+VERSION+"/lasla-plus.tar",
-         "model.tar")
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION +
+         "/lasla-plus-Mood_Tense_Voice.tar",
+         "Mood_Tense_Voice.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Gend.tar",
+         "Gend.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Person.tar",
+         "Person.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Deg.tar",
+         "Deg.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-lemma.tar",
+         "lemma.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-pos.tar",
+         "pos.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Numb.tar",
+         "Numb.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Dis.tar",
+         "Dis.tar"),
+    File("https://github.com/PonteIneptique/latin-lasla-models/releases/download/" + VERSION + "/lasla-plus-Case.tar",
+         "Case.tar")
 ]
 
-Models = "<{},lemma,Deg,Numb,Person,Mood_Tense_Voice,Case,Gend,Dis,pos>".format(get_path("lasla", "model.tar"))
+Models = "".join([
+    "<{},Mood_Tense_Voice>".format(get_path("lasla", "Mood_Tense_Voice")),
+    "<{},Gend>".format(get_path("lasla", "Gend")),
+    "<{},Person>".format(get_path("lasla", "Person")),
+    "<{},Deg>".format(get_path("lasla", "Deg")),
+    "<{},lemma>".format(get_path("lasla", "lemma")),
+    "<{},pos>".format(get_path("lasla", "pos")),
+    "<{},Numb>".format(get_path("lasla", "Numb")),
+    "<{},Dis>".format(get_path("lasla", "Dis")),
+    "<{},Case>".format(get_path("lasla", "Case"))
+])

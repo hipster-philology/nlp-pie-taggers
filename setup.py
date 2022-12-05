c5dd3fa1000c4d27b8baca3e67fb0ee15c22b0fb
Thibault Clérice <leponteineptique@gmail.com>
2022-12-05 20:37:31 +0100

    (0.0.41) Upgrade models links for FRO and FREEM

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DESCRIPTION = "Extension for nlp-pie package"
 URL = 'https://github.com/hipster-philology/nlp-pie-taggers'
 AUTHOR = 'Thibault Clérice'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.0.40"
+VERSION = "0.0.41"
 
 # What packages are required for this module to be executed?
 

69f896d90670d6865b7a25edabf4716180d77b69
Thibault Clérice <leponteineptique@gmail.com>
2023-04-27 12:01:51 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DESCRIPTION = "Extension for nlp-pie package"
 URL = 'https://github.com/hipster-philology/nlp-pie-taggers'
 AUTHOR = 'Thibault Clérice'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # What packages are required for this module to be executed?
 
